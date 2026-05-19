d9f52f75050a975b79e73997dcd325c825008abb
cseljatib <cseljatib@gmail.com>
2026-05-19 19:35:53 -0400

    script varpredictora factor mas explicativos

--- regpredfactcont1_ecol.r
+++ regpredfactcont1_ecol.r
@@ -6,8 +6,8 @@
 ##-║ Mas detalles:  Ajuste del modelo con variable dummy como      ║
 ## ║  predictor.                                                   ║
 ## ║                                                               ║
-##*║ Ejemplo: Datos de ingreso total corregido segun               ║
-##*║ encuesta (casen).                                             ║
+##*║ Ejemplo: Datos del peso de osos (bearscomp2)                  ║
+##*║                                                               ║
 ##-║---------------------------------------------------------------║
 ## ║                                                               ║
 ##>║ Profesor: Christian Salas Eljatib                             ║
@@ -19,42 +19,29 @@
 ##! I. Datos para ejemplo
 ##- ===================================
 library(datana)
-head(casen)
-##- revise la metadata al activar la siguiente linea
-#?casen
-df<-casen
-dim(df)
-n.ori<-nrow(df);n.ori
+df <- datana::bearscomp2
+head(df)
 
-descstat(data=df,y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,edad>=18),y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,activ=="Ocupados"),y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,edad >=18 & ytotcor>0),y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,edad >=18 & ytotcor>0&activ=="Ocupados"),y=c("edad","ytot","ytotcor"))
+tapply(df$peso,df$sexo,length)
 
-dfx<-subset(df,edad >=18 & ytotcor>0&activ=="Ocupados")
-boxplot(dfx$ytotcor)
-df.ori<-df
+is.factor(df$sexo.name)
+df$sexo.nombre<-as.factor(df$sexo.nombre)
+means.g <- tapply(df$peso,df$sexo.nombre,mean)
+means.g
 
-##- filtro, emplear solo los datos para adultos con ingresos
-df<-subset(df.ori, edad >=18 & ytotcor>0&activ=="Ocupados")
-dim(df)
+sds.g <- tapply(df$peso,df$sexo.nombre,sd)
+sds.g
 
-conteo.comunas <- table(df$comuna);conteo.comunas
+ns.g <- tapply(df$peso,df$sexo.nombre,length)
+ns.g
 
-# al menos 10 observaciones
-comunas.validas <- names(conteo.comunas[conteo.comunas >= 4])
-comunas.validas
+boxplot(df$peso ~df$sexo.nombre, ylab="Peso (Kg)",
+xlab="Sexo")
 
-# filtrando los datos 
-df <- subset(df, as.character(comuna) %in% comunas.validas)
-dim(df)
-str(df)
-sum(table(df$comuna)) == nrow(df) # Tiene que coincidir
-
+descstat(data=df,y=c("peso"),factvar = "sexo.nombre")
 
 ##? variable aleatoria de interes: la edad
-df$vary<-df$ytotcor
+df$vary<-df$peso
 
 ##! grafico de distribucion
 boxplot(df$vary)
@@ -66,7 +53,7 @@ histogram(~vary,data=df)
 
 
 ##* todo junto
-histbxp(df$vary,varlab = "Ingreso total ($/mes)")
+histbxp(df$vary,varlab = "Peso (Kg)")
 
 ##! Estadistica descriptiva
 descstat(data=df,y=c("vary"))
@@ -74,23 +61,13 @@ descstat(data=df,y=c("vary"),full = TRUE,eng=FALSE)
 
 
 ##! analizemos una variable categorica
-table(df$sexo)
+table(df$sexo.nombre)
 ##creando una nueva variable
-extractLeft("1. Hombre",1)
-as.numeric(extractLeft("1. Hombre",1))
-
-df$sex.code<-as.numeric(extractLeft(df$sexo,1))
-table(df$sex.code)
-head(df)
-deleteLeft("1. Hombre",3)
-deleteLeft("2. Mujer",3)
-df$sexo<-deleteLeft(df$sexo,3)
-table(df$sexo)
 
 color.aca <- c("blue","red")
-boxplot(vary ~sexo, data=subset(df,vary<3000000), col =color.aca, ylab="Ing. total ($/mes)")
+boxplot(vary ~sexo, data=df, col =color.aca, ylab="Peso (kg)")
 
-descstat(data=df,y=c("vary"),factvar = "sexo")
+descstat(data=df,y=c("vary"),factvar = "sexo.nombre")
 
 
 
@@ -100,7 +77,7 @@ descstat(data=df,y=c("vary"),factvar = "sexo")
 ##+ Primer modelo, solo el factor como predictor
 #Identica sintaxis que para ajuste de modelos de regresion lineal
 # para lo cual empleamos la funcion lm().
-m1 <- lm(vary ~ sexo, data=df)
+m1 <- lm(vary ~ sexo.nombre, data=df)
 summary(m1)
 
 ##- Valor esperado del modelo ajustado
@@ -111,9 +88,9 @@ coef(m1)
 b0.hat<-coef(m1)[1]
 b1.hat<-coef(m1)[2]
 
-##para el nivel "Mujer" del factor "sexo"
+##para el nivel "Hembra" del factor "sexo.nombre"
 b0.hat+b1.hat*1
-##y para el nivel "Hombre" del factor "sexo"
+##y para el nivel "Macho" del factor "sexo.nombre"
 b0.hat+b1.hat*0
 
 
@@ -121,16 +98,17 @@ b0.hat+b1.hat*0
 anova(m1)
 
 ##* Que tan bueno es este modelo
-100*summary(m1)$sigma/mean(df$vary)
+100*summary(m1)$sigma/mean(df$peso)
 
 ##+ Calculo de estadisticos de prediccion
-predstat(obs=df$vary,pre=fitted(m1),want.percent = T)
+predstat(obs=df$peso,pre=fitted(m1),want.percent = T)
 
 
 ##- ===================================
 ##! IV. Ajuste de modelo 2 -- Una variable continua como predictor
 ##- ===================================
 ##+ Primer modelo, solo la edad como predictor
+plot(vary ~ edad, data=df)
 m2 <- lm(vary ~ edad, data=df)
 summary(m2)
 
@@ -138,9 +116,9 @@ summary(m2)
 b0.hat2<-coef(m2)[1]
 b1.hat2<-coef(m2)[2]
 
-##para una edad de 30 anhos
-b0.hat2+b1.hat2*30
-##para una edad de 50 anhos
+##para una edad de 48 meses
+b0.hat2+b1.hat2*48
+##para una edad de 50 meses
 b0.hat2+b1.hat2*50
 
 ##* Que tan bueno es este modelo
@@ -149,8 +127,6 @@ predstat(obs=df$vary,pre=fitted(m2),want.percent = T)
 ##+ Cuadro "analisis de varianza del modelo ajustado"
 anova(m2)
 
-
-
 ##- ===================================
 ##! IV. Ajuste de modelo 3 -- dos variables predictoras:
 ## una continua y otro factor
@@ -158,14 +134,15 @@ anova(m2)
 ##* Analicemos graficamente
 
 require(lattice)
-histogram(~vary|sexo, data=df)
+histogram(~vary|sexo.nombre, data=df)
 
 xyplot(vary~edad| sexo, data=df)
 
-xyplot(vary~edad,groups = sexo, data=df,auto.key = TRUE)
+xyplot(vary~edad,groups = sexo.nombre, data=df,auto.key = TRUE)
+
 
 ##+ Edad y el factor "Sexo" como predictores
-m3 <- lm(vary ~ edad+sexo, data=df)
+m3 <- lm(vary ~ edad+sexo.nombre, data=df)
 ## la sintaxis es la de un modelo de reg. lineal multiple
 summary(m3)
 
@@ -175,18 +152,17 @@ b0.hat3<-coef(m3)[1]
 b1.hat3<-coef(m3)[2]
 b2.hat3<-coef(m3)[3]
 
-##? para una edad de 30 anhos
-##  ..... para mujer?
-b0.hat3+b1.hat3*30+b2.hat3*1
-##  ..... para hombre?
-b0.hat3+b1.hat3*30+b2.hat3*0
+##? un oso de 48 meses
+##  ..... para hembra?
+b0.hat3+b1.hat3*48+b2.hat3*1
+##  ..... para macho?
+b0.hat3+b1.hat3*48+b2.hat3*0
 
-##? para una edad de 50 anhos
-##  ..... para mujer?
+##? un oso de 50 meses
+##  ..... para hembra?
 b0.hat3+b1.hat3*50+b2.hat3*1
-##  ..... para hombre?
+##  ..... para macho?
 b0.hat3+b1.hat3*50+b2.hat3*0
-
 
 
 ##* Que tan bueno es este modelo
@@ -203,10 +179,6 @@ anova(m2,m3)
 ##+======================================================
 ##! Grafico de comportamiento para este modelo
 ##+======================================================
-col.list <- rep(0, length(df$sexo))
-col.list[df$sexo=="Hombre"] <- "red"
-col.list[df$sexo=="Mujer"] <- "blue"
-
 coef <- coefficients(m3)
 plot(df$edad, df$vary, col=col.list, ylab="Ingreso ($/mes)", xlab="Edad (años)")
 abline(coef["(Intercept)"] + coef["sexoMujer"],
@@ -216,8 +188,78 @@ coef["edad"], col = "blue")
 legend('topright',unique(df$sexo),col=unique(col.list),pch=1)
 
 
-## que le parecen los valores esperados para cada nivel en relacion
-## a la dispersion de los datos observados.
+
+##- ===================================
+##! V. Ajuste de modelo 3b -- otra variante (varpred continua y factor)
+##- ===================================
+##+ Otra variante: Edad y el factor "Sexo" como predictores
+m3b <- lm(vary ~ edad:sexo, data=df)
+summary(m3b)
+
+
+##- Valor esperado del modelo ajustado
+b0.hat3b<-coef(m3b)[1]
+b1.hat3b<-coef(m3b)[2]
+b2.hat3b<-coef(m3b)[3]
+
+##? para una edad de 30 anhos
+##  ..... para mujer?
+b0.hat3b+b1.hat3b*30*0+b2.hat3b*30*1
+##  ..... para hombre?
+b0.hat3b+b1.hat3b*30*1+b2.hat3b*30*0
+
+##? para una edad de 50 anhos
+##  ..... para mujer?
+b0.hat3b+b1.hat3b*50*0+b2.hat3b*50*1
+##  ..... para hombre?
+b0.hat3b+b1.hat3b*50*1+b2.hat3b*50*0
+
+
+##* Que tan bueno es este modelo
+predstat(obs=df$vary,pre=fitted(m3b),want.percent = T)
+
+
+##! test de F-parcial
+anova(m2,m3b)
+
+
+##+ Ultima variante: Edad y el factor "Sexo" como predictores
+m3c <- lm(vary ~ edad*sexo, data=df)
+summary(m3c)
+
+
+##- Valor esperado del modelo ajustado
+b0.hat3c<-coef(m3c)[1]
+b1.hat3c<-coef(m3c)[2]
+b2.hat3c<-coef(m3c)[3]
+b3.hat3c<-coef(m3c)[4]
+
+##? para una edad de 30 anhos
+##  ..... para mujer?
+b0.hat3c + b1.hat3c*30 + b2.hat3c*1 + b3.hat3c*30*1
+##  ..... para hombre?
+b0.hat3c + b1.hat3c*30 + b2.hat3c*0 + b3.hat3c*30*0
+
+##? para una edad de 50 anhos
+##  ..... para mujer?
+b0.hat3c + b1.hat3c*50 + b2.hat3c*1 + b3.hat3c*50*1
+##  ..... para hombre?
+b0.hat3c + b1.hat3c*50 + b2.hat3c*0 + b3.hat3c*50*0
+
+##* Que tan bueno es este modelo
+predstat(obs=df$vary,pre=fitted(m3c),want.percent = T)
+
+summary(m3c)
+##! test de F-parcial
+anova(m2,m3c)
+
+coef <- coefficients(m3c)
+plot(vary~edad, data=df, col=col.list, ylab="Ingreso ($/mes)", xlab="Edad (años)")
+abline(coef["(Intercept)"] + coef["sexoMujer"],
+coef["edad"]+coef["edad:sexoMujer"], col = "red",lwd=4)
+abline(coef["(Intercept)"],
+coef["edad"], col = "blue",lwd=2)
+legend('topleft',unique(df$sexo),col=unique(col.list),pch=1)
 
 #-╔═════════════════╗
 #-║ Fin del script! ║
--- regpredfactcont2_ecol.r
+++ regpredfactcont2_ecol.r
@@ -7,8 +7,8 @@
 ## ║  predictor y sus diferentes variantes para ser incorporadas   ║
 ## ║  en el modelo estadistico.                                    ║
 ## ║                                                               ║
-##*║ Ejemplo: Datos de ingreso total corregido segun               ║
-##*║ encuesta (casen).                                             ║
+##*║ Ejemplo: Datos de la variable "peso" (biomasa) de osos en     ║
+##*║ Norteamerica (bearscomp2)                                     ║
 ##-║---------------------------------------------------------------║
 ## ║                                                               ║
 ##>║ Profesor: Christian Salas Eljatib                             ║
@@ -16,46 +16,34 @@
 ##*║ Web: https://eljatib.com                                      ║
 ##!╚═══════════════════════════════════════════════════════════════╝
 
+
 ##- ===================================
 ##! I. Datos para ejemplo
 ##- ===================================
 library(datana)
-head(casen)
-##- revise la metadata al activar la siguiente linea
-#?casen
-df<-casen
-dim(df)
-n.ori<-nrow(df);n.ori
+df <- datana::bearscomp2
+head(df)
 
-descstat(data=df,y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,edad>=18),y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,activ=="Ocupados"),y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,edad >=18 & ytotcor>0),y=c("edad","ytot","ytotcor"))
-descstat(data=subset(df,edad >=18 & ytotcor>0&activ=="Ocupados"),y=c("edad","ytot","ytotcor"))
+tapply(df$peso,df$sexo,length)
 
-dfx<-subset(df,edad >=18 & ytotcor>0&activ=="Ocupados")
-boxplot(dfx$ytotcor)
-df.ori<-df
+is.factor(df$sexo.nombre)
+df$sexo.nombre<-as.factor(df$sexo.nombre)
+means.g <- tapply(df$peso,df$sexo.nombre,mean)
+means.g
 
-##- filtro, emplear solo los datos para adultos con ingresos
-df<-subset(df.ori, edad >=18 & ytotcor>0&activ=="Ocupados")
-dim(df)
+sds.g <- tapply(df$peso,df$sexo.nombre,sd)
+sds.g
 
-conteo.comunas <- table(df$comuna);conteo.comunas
+ns.g <- tapply(df$peso,df$sexo.nombre,length)
+ns.g
 
-# al menos 10 observaciones
-comunas.validas <- names(conteo.comunas[conteo.comunas >= 4])
-comunas.validas
+boxplot(df$peso ~df$sexo.nombre, ylab="Peso (Kg)",
+xlab="Sexo")
 
-# filtrando los datos 
-df <- subset(df, as.character(comuna) %in% comunas.validas)
-dim(df)
-str(df)
-sum(table(df$comuna)) == nrow(df) # Tiene que coincidir
-
+descstat(data=df,y=c("peso"),factvar = "sexo.nombre")
 
 ##? variable aleatoria de interes: la edad
-df$vary<-df$ytotcor
+df$vary<-df$peso
 
 ##! grafico de distribucion
 boxplot(df$vary)
@@ -67,7 +55,7 @@ histogram(~vary,data=df)
 
 
 ##* todo junto
-histbxp(df$vary,varlab = "Ingreso total ($/mes)")
+histbxp(df$vary,varlab = "Peso (Kg)")
 
 ##! Estadistica descriptiva
 descstat(data=df,y=c("vary"))
@@ -75,23 +63,13 @@ descstat(data=df,y=c("vary"),full = TRUE,eng=FALSE)
 
 
 ##! analizemos una variable categorica
-table(df$sexo)
+table(df$sexo.nombre)
 ##creando una nueva variable
-extractLeft("1. Hombre",1)
-as.numeric(extractLeft("1. Hombre",1))
-
-df$sex.code<-as.numeric(extractLeft(df$sexo,1))
-table(df$sex.code)
-head(df)
-deleteLeft("1. Hombre",3)
-deleteLeft("2. Mujer",3)
-df$sexo<-deleteLeft(df$sexo,3)
-table(df$sexo)
 
 color.aca <- c("blue","red")
-boxplot(vary ~sexo, data=subset(df,vary<3000000), col =color.aca, ylab="Ing. total ($/mes)")
+boxplot(vary ~sexo, data=df, col =color.aca, ylab="Peso (kg)")
 
-descstat(data=df,y=c("vary"),factvar = "sexo")
+descstat(data=df,y=c("vary"),factvar = "sexo.nombre")
 
 
 
@@ -101,7 +79,7 @@ descstat(data=df,y=c("vary"),factvar = "sexo")
 ##+ Primer modelo, solo el factor como predictor
 #Identica sintaxis que para ajuste de modelos de regresion lineal
 # para lo cual empleamos la funcion lm().
-m1 <- lm(vary ~ sexo, data=df)
+m1 <- lm(vary ~ sexo.nombre, data=df)
 summary(m1)
 
 ##- Valor esperado del modelo ajustado
@@ -112,9 +90,9 @@ coef(m1)
 b0.hat<-coef(m1)[1]
 b1.hat<-coef(m1)[2]
 
-##para el nivel "Mujer" del factor "sexo"
+##para el nivel "Hembra" del factor "sexo.nombre"
 b0.hat+b1.hat*1
-##y para el nivel "Hombre" del factor "sexo"
+##y para el nivel "Macho" del factor "sexo.nombre"
 b0.hat+b1.hat*0
 
 
@@ -122,16 +100,17 @@ b0.hat+b1.hat*0
 anova(m1)
 
 ##* Que tan bueno es este modelo
-100*summary(m1)$sigma/mean(df$vary)
+100*summary(m1)$sigma/mean(df$peso)
 
 ##+ Calculo de estadisticos de prediccion
-predstat(obs=df$vary,pre=fitted(m1),want.percent = T)
+predstat(obs=df$peso,pre=fitted(m1),want.percent = T)
 
 
 ##- ===================================
-##! IV. Ajuste de modelo 2 -- Una variable continua como predictor
+##! IV. Ajuste de modelo 2 -- Una variable continua como predictora
 ##- ===================================
 ##+ Primer modelo, solo la edad como predictor
+plot(vary ~ edad, data=df)
 m2 <- lm(vary ~ edad, data=df)
 summary(m2)
 
@@ -139,9 +118,9 @@ summary(m2)
 b0.hat2<-coef(m2)[1]
 b1.hat2<-coef(m2)[2]
 
-##para una edad de 30 anhos
-b0.hat2+b1.hat2*30
-##para una edad de 50 anhos
+##para una edad de 48 meses
+b0.hat2+b1.hat2*48
+##para una edad de 50 meses
 b0.hat2+b1.hat2*50
 
 ##* Que tan bueno es este modelo
@@ -159,15 +138,14 @@ anova(m2)
 ##* Analicemos graficamente
 
 require(lattice)
-histogram(~vary|sexo, data=df)
+histogram(~vary|sexo.nombre, data=df)
 
-xyplot(vary~edad| sexo, data=df)
+xyplot(vary~edad| sexo.nombre, data=df)
 
-xyplot(vary~edad,groups = sexo, data=df,auto.key = TRUE)
+xyplot(vary~edad,groups = sexo.nombre, data=df,auto.key = TRUE)
 
-
-##+ Edad y el factor "Sexo" como predictores
-m3 <- lm(vary ~ edad+sexo, data=df)
+##+ Edad y el factor "sexo.nombre" como predictores
+m3 <- lm(vary ~ edad+sexo.nombre, data=df)
 ## la sintaxis es la de un modelo de reg. lineal multiple
 summary(m3)
 
@@ -177,16 +155,16 @@ b0.hat3<-coef(m3)[1]
 b1.hat3<-coef(m3)[2]
 b2.hat3<-coef(m3)[3]
 
-##? para una edad de 30 anhos
-##  ..... para mujer?
-b0.hat3+b1.hat3*30+b2.hat3*1
-##  ..... para hombre?
-b0.hat3+b1.hat3*30+b2.hat3*0
+##? un oso de 48 meses
+##  ..... para hembra?
+b0.hat3+b1.hat3*48+b2.hat3*1
+##  ..... para macho?
+b0.hat3+b1.hat3*48+b2.hat3*0
 
-##? para una edad de 50 anhos
-##  ..... para mujer?
+##? un oso de 50 meses
+##  ..... para hembra?
 b0.hat3+b1.hat3*50+b2.hat3*1
-##  ..... para hombre?
+##  ..... para macho?
 b0.hat3+b1.hat3*50+b2.hat3*0
 
 
@@ -205,18 +183,17 @@ anova(m2,m3)
 ##+======================================================
 ##! Grafico de comportamiento para este modelo
 ##+======================================================
+col.list <- rep(0, length(df$sexo.nombre))
+col.list[df$sexo.nombre=="Macho"] <- "red"
+col.list[df$sexo.nombre=="Hembra"] <- "blue"
+
 coef <- coefficients(m3)
-
-col.list <- rep(0, length(df$sexo))
-col.list[df$sexo=="Hombre"] <- "red"
-col.list[df$sexo=="Mujer"] <- "blue"
-
-plot(df$edad, df$vary, col=col.list, ylab="Ingreso ($/mes)", xlab="Edad (años)")
-abline(coef["(Intercept)"] + coef["sexoMujer"],
+plot(df$edad, df$vary, col=col.list, ylab="Peso (kg)", xlab="Edad (meses)")
+abline(coef["(Intercept)"] + coef["sexo.nombreHembra"],
 coef["edad"], col = "red")
 abline(coef["(Intercept)"],
 coef["edad"], col = "blue")
-legend('topright',unique(df$sexo),col=unique(col.list),pch=1)
+legend('topright',levels(df$sexo.nombre),col=col.list,pch=1)
 ## note que aca solo varian los interceptos entre los niveles del
 ##  factor.
 
@@ -233,8 +210,8 @@ legend('topright',unique(df$sexo),col=unique(col.list),pch=1)
 ##! b) Ajuste de modelo con pendientes diferentes entre niveles
 ## del factor
 ##- ===================================
-##+ recuerde, las variables predictoras son: Edad y el factor "Sexo"
-m3b <- lm(vary ~ edad:sexo, data=df)
+##+ recuerde, las variables predictoras son: Edad y el factor "Sexo.Nombre"
+m3b <- lm(vary ~ edad:sexo.nombre, data=df)
 summary(m3b)
 
 
@@ -243,17 +220,18 @@ b0.hat3b<-coef(m3b)[1]
 b1.hat3b<-coef(m3b)[2]
 b2.hat3b<-coef(m3b)[3]
 
-##? para una edad de 30 anhos
-##  ..... para mujer?
-b0.hat3b+b1.hat3b*30*0+b2.hat3b*30*1
-##  ..... para hombre?
-b0.hat3b+b1.hat3b*30*1+b2.hat3b*30*0
+##? oso de 48 meses
+##  ..... para Hembra?
+b0.hat3b+b1.hat3b*48*0+b2.hat3b*48*1
+##  ..... para Macho?
+b0.hat3b+b1.hat3b*48*1+b2.hat3b*48*0
 
-##? para una edad de 50 anhos
-##  ..... para mujer?
-b0.hat3b+b1.hat3b*50*0+b2.hat3b*50*1
-##  ..... para hombre?
-b0.hat3b+b1.hat3b*50*1+b2.hat3b*50*0
+
+##? oso de 80 meses
+##  ..... para Hembra?
+b0.hat3b+b1.hat3b*80*0+b2.hat3b*80*1
+##  ..... para Macho?
+b0.hat3b+b1.hat3b*80*1+b2.hat3b*80*0
 
 
 ##* Que tan bueno es este modelo
@@ -269,8 +247,8 @@ anova(m2,m3b)
 ##! b) Ajuste de modelo con interceptos y pendientes diferentes entre
 ## niveles del factor
 ##- ===================================
-##+ recuerde, las variables predictoras son: Edad y el factor "Sexo"
-m3c <- lm(vary ~ edad*sexo, data=df)
+##+ recuerde, las variables predictoras son: Edad y el factor "Sexo.Nombre"
+m3c <- lm(vary ~ edad*sexo.nombre, data=df)
 summary(m3c)
 
 
@@ -280,17 +258,17 @@ b1.hat3c<-coef(m3c)[2]
 b2.hat3c<-coef(m3c)[3]
 b3.hat3c<-coef(m3c)[4]
 
-##? para una edad de 30 anhos
-##  ..... para mujer?
-b0.hat3c + b1.hat3c*30 + b2.hat3c*1 + b3.hat3c*30*1
+##? un oso de 48 meses 
+##  ..... para Hembra?
+b0.hat3c + b1.hat3c*48 + b2.hat3c*1 + b3.hat3c*48*1
 ##  ..... para hombre?
-b0.hat3c + b1.hat3c*30 + b2.hat3c*0 + b3.hat3c*30*0
+b0.hat3c + b1.hat3c*48 + b2.hat3c*0 + b3.hat3c*48*0
 
-##? para una edad de 50 anhos
-##  ..... para mujer?
-b0.hat3c + b1.hat3c*50 + b2.hat3c*1 + b3.hat3c*50*1
+##? un oso de 80 meses 
+##  ..... para Hembra?
+b0.hat3c + b1.hat3c*80 + b2.hat3c*1 + b3.hat3c*80*1
 ##  ..... para hombre?
-b0.hat3c + b1.hat3c*50 + b2.hat3c*0 + b3.hat3c*50*0
+b0.hat3c + b1.hat3c*80 + b2.hat3c*0 + b3.hat3c*80*0
 
 ##* Que tan bueno es este modelo
 predstat(obs=df$vary,pre=fitted(m3c),want.percent = T)
@@ -305,12 +283,12 @@ anova(m2,m3c)
 ## pendientes diferentes entre niveles del factor.
 ##+======================================================
 coef <- coefficients(m3c)
-plot(vary~edad, data=df, col=col.list, ylab="Ingreso ($/mes)", xlab="Edad (años)")
-abline(coef["(Intercept)"] + coef["sexoMujer"],
-coef["edad"]+coef["edad:sexoMujer"], col = "red",lwd=4)
+plot(vary~edad, data=df, col=col.list, ylab="Peso (kg)", xlab="Edad (meses)")
+abline(coef["(Intercept)"] + coef["sexo.nombreHembra"],
+coef["edad"]+coef["edad:sexo.nombreHembra"], col = "red",lwd=4)
 abline(coef["(Intercept)"],
 coef["edad"], col = "blue",lwd=2)
-legend('topleft',unique(df$sexo),col=unique(col.list),pch=1)
+legend('topleft',levels(df$sexo.nombre),col=col.list,pch=1)
 
 
 ##? ===================
